7d7f3f4ba9c8e58bdd02be5d356cc3d76dec0e39
Pauldevtech <pablousa03@hotmail.com>
2025-03-27 19:44:20 +0100

    Disable dummy footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,6 +11,11 @@ import {
 } from './styles';
 
 const Footer: FC = () => {
+  // Add this handler to prevent default link behavior
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <FooterWrapper role="contentinfo">
       <FooterContainer>
@@ -25,26 +30,26 @@ const Footer: FC = () => {
         <FooterNavigation>
           <FooterSection>
             <SectionTitle>Services</SectionTitle>
-            <FooterLink href="/converter">Converter</FooterLink>
-            <FooterLink href="/rates">Exchange Rates</FooterLink>
-            <FooterLink href="/historical">Historical Data</FooterLink>
-            <FooterLink href="/alerts">Rate Alerts</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Converter</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Exchange Rates</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Historical Data</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Rate Alerts</FooterLink>
           </FooterSection>
 
           <FooterSection>
             <SectionTitle>Company</SectionTitle>
-            <FooterLink href="/about">About Us</FooterLink>
-            <FooterLink href="/contact">Contact</FooterLink>
-            <FooterLink href="/blog">Blog</FooterLink>
-            <FooterLink href="/careers">Careers</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>About Us</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Contact</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Blog</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Careers</FooterLink>
           </FooterSection>
 
           <FooterSection>
             <SectionTitle>Legal</SectionTitle>
-            <FooterLink href="/privacy">Privacy Policy</FooterLink>
-            <FooterLink href="/terms">Terms of Service</FooterLink>
-            <FooterLink href="/cookies">Cookie Policy</FooterLink>
-            <FooterLink href="/disclaimer">Disclaimer</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Privacy Policy</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Terms of Service</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Cookie Policy</FooterLink>
+            <FooterLink href="#" onClick={handleClick}>Disclaimer</FooterLink>
           </FooterSection>
         </FooterNavigation>
       </FooterContainer>
